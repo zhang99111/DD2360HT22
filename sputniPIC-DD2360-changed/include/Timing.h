dd9840fec265c1bd53679faf93164b49182d0315
zhang99111 <119762939+zhang99111@users.noreply.github.com>
2022-12-15 17:37:49 +0100

    delet

--- sputniPIC-DD2360-changed/include/Timing.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIMING_H
-#define TIMING_H
-
-#include <sys/time.h>
-
-// return time in second
-double cpuSecond();
-
-#endif
--- sputniPIC-DD2360-changed/src/Timing.cpp
+++
@@ -1,8 +0,0 @@
-#include "Timing.h"
-
-double cpuSecond()
-{
-    struct timeval tp;
-    gettimeofday(&tp, nullptr);
-    return ((double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
-}
